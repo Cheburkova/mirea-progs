0ea4ee6c87a730899784f08a45f2ac9d538707e7
Cheburkova <90936335+Cheburkova@users.noreply.github.com>
2021-12-22 20:12:02 +0300

    Update task_7.jl

--- task_7.jl
+++ task_7.jl
@@ -1,10 +1,9 @@
 using HorizonSideRobots
 
-function corner(r::Robot)
-    s=0::Int
-    n=0::Int
-    o=0::Int
-    w=0::Int
+function chessall(r::Robot)
+    s = 0
+    w = 0
+
     while !isborder(r,Sud)
         move!(r,Sud)
         s+=1
@@ -13,61 +12,76 @@ function corner(r::Robot)
         move!(r,West)
         w+=1
     end
+
     if w%2==0
         shax(r)
     else
         move!(r,Ost)
         shax(r)
     end
+
     while !isborder(r,Sud)
         move!(r,Sud)
     end
     while !isborder(r,West)
         move!(r,West)
     end
-    for o in 1:w
+
+    for a in 1:w
         move!(r,Ost)
     end
-    for n in 1:s
+    for a in 1:s
         move!(r,Nord)
     end
 end
 
-function shax(r)
+function shax(r::Robot)
     putmarker!(r)
     i=0
+
     while !isborder(r,Nord)
-        stripe(r,Ost)
-        obr(r,i)
-        if !isborder(r,Nord)
+        if !isborder(r,Sud)
             move!(r,Nord)
-            obr(r,i)
-            stripe(r,West)
         end
-    end
-end
-
-function obr(r,i)
-    if i == 1
-        i = 0
-    else
-        i = 1
-        putmarker!(r)
-    end
-end
-
-function stripe(r,side)
-    while !isborder(r,side)
         if i == 1
-            move!(r,side)
             i = 0
         else
-            move!(r,side)
-            putmarker!(r)
             i = 1
+            putmarker!(r)
         end
+
+        while !isborder(r,Ost)
+            if i == 1
+                move!(r,Ost)
+                i = 0
+            else
+                move!(r,Ost)
+                putmarker!(r)
+                i = 1
+            end
+        end
+
+        if !isborder(r,Nord)
+            move!(r,Nord)
+            if i == 1
+                i = 0
+            else
+                i = 1
+                putmarker!(r)
+            end
+            while !isborder(r,West)
+                if i == 1
+                    move!(r,West)
+                    i = 0
+                else
+                    move!(r,West)
+                    putmarker!(r)
+                    i = 1
+                end
+            end
+        end
+
     end
-    move!(r,Nord)
 end
 
-corner(r)
+chessall(r)
